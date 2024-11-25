1fdafdce5813d14077f93e7a3752c53b095c515f
Gazi Md Rakibul Hasan <ens20ghn@cs.umu.se>
2024-11-25 20:17:42 +0100

    not sure if node is correctly implemented

--- OU3/Resources/TestNode/src/bin/node.c
+++ OU3/Resources/TestNode/src/bin/node.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <netdb.h>
+#include <arpa/inet.h>
 #include "../../../pdu.h"
 
 /*
@@ -47,7 +48,7 @@ int main(int argc, char* argv[]) {
     }
 
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
 
     status = getaddrinfo(argv[1], argv[2], &hints, &res);
@@ -57,27 +58,71 @@ int main(int argc, char* argv[]) {
     }
 
     int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-    int bind_status = bind(sockfd, res->ai_addr, res->ai_addrlen);
-
     if (sockfd == -1) {
-        perror("socket");
+        perror("socket ");
         return 1;
     }
 
     // send a STUN_LOOKUP PDU to the tracker
-    struct STUN_LOOKUP_PDU stun_lookup_pdu;
-    stun_lookup_pdu.type = STUN_LOOKUP;
-    sendto(sockfd, &stun_lookup_pdu, sizeof(stun_lookup_pdu), 0, res->ai_addr, res->ai_addrlen);
-    printf("Tracker address: %u\n", stun_lookup_pdu);
+    struct STUN_LOOKUP_PDU stun_lookup;
+    stun_lookup.type = STUN_LOOKUP;
+    int send_status = sendto(sockfd, &stun_lookup, sizeof(stun_lookup), 0, res->ai_addr, res->ai_addrlen);
+    if(send_status == -1) {
+        perror("it failed to send the STUN_LOOKUP PDU");
+        return 1;
+    }
 
     // receive a STUN_RESPONSE PDU from the tracker
-    struct STUN_RESPONSE_PDU stun_response_pdu;
-    recvfrom(sockfd, &stun_response_pdu, sizeof(stun_response_pdu), 0, res->ai_addr, &res->ai_addrlen);
-
-    if(stun_response_pdu.type == STUN_RESPONSE) {
-        printf("Public address: %u\n", stun_response_pdu.address);
+    struct STUN_RESPONSE_PDU stun_response;
+    int rcv_status = recvfrom(sockfd, &stun_response, sizeof(stun_response), 0, res->ai_addr, &res->ai_addrlen);
+    if(rcv_status == -1) {
+        perror("it failed to receive the STUN_RESPONSE PDU");
+        return 1;
     }
+
+    
+    if (stun_response.type == STUN_RESPONSE) {
+        struct in_addr addr = {stun_response.address};
+        printf("Public IP: %s\n", inet_ntoa(addr));
+    }
+
+
+    // it is time to node send NET_GET_NODE to the tracker
+    struct NET_GET_NODE_PDU net_get_node;
+    net_get_node.type = NET_GET_NODE;
+    
+    send_status = sendto(sockfd, &net_get_node, sizeof(net_get_node), 0, res->ai_addr, res->ai_addrlen);
+    if(send_status == -1) {
+        perror("it failed to send the NET_GET_NODE PDU");
+        return 1;
+    }
+
+    struct NET_GET_NODE_RESPONSE_PDU net_get_node_response;
+    rcv_status = recvfrom(sockfd, &net_get_node_response, sizeof(net_get_node_response), 0, res->ai_addr, &res->ai_addrlen);
+    if(rcv_status == -1) {
+        perror("it failed to receive the NET_GET_NODE_RESPONSE PDU");
+        return 1;
+    }
+
+
+    if(net_get_node_response.type == NET_GET_NODE_RESPONSE) {
+        struct in_addr addr;
+        addr.s_addr = net_get_node_response.address;
+        printf("Node IP: %s\n", inet_ntoa(addr));
+        printf("Node Port: %d\n", ntohs(net_get_node_response.port));
+    }
+
+
+    while(1){
+        if(net_get_node_response.address == 0 && net_get_node_response.port == 0) {
+        printf("The network is empty\n");
+    }
+
+    
+    // since it is empty, we can sent 
+
+    freeaddrinfo(res);
+    close(sockfd);
 
 
     return 0;
