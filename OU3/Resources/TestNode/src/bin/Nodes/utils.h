50de09c8e9f215c33bcfae917edc51eb9c05fb85
Gazi Md Rakibul Hasan <ens20ghn@cs.umu.se>
2024-12-11 18:57:44 +0100

    state 15, state 11 is working the way it suppose to be and another fucntion added to merge range

--- OU3/Resources/TestNode/src/bin/Nodes/utils.h
+++ OU3/Resources/TestNode/src/bin/Nodes/utils.h
@@ -2,6 +2,13 @@
 #define UTILS_H
 
 #include "node.h"
+#include "communication.h"
+#include "join_network.h"
+#include "signal_handler.h"
+#include "utils.h"
+#include "initialization.h"
+
+struct Node;
 
 uint8_t calulate_hash_span(uint8_t start, uint8_t end);
 void update_hash_range(void* n, uint8_t new_range_start, uint8_t new_range_end);
